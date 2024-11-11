1dc9759017bd88b7998c75fe34897c10c7e3e5f0
FaconFlorian <florian.facon@student.vinci.be>
2024-11-11 14:43:32 +0100

    add: test => minute 13

--- spec/ApppSpec.js
+++ spec/ApppSpec.js
@@ -82,5 +82,13 @@ describe("berlinClock function should return time", function (){
         expect(result).toBe("1100");
     });
 
+    it("Test minute 13",function (){
+        let main = new Main();
+
+        const result = main.berlinClock(13);
+
+        expect(result).toBe("1110");
+    });
+
 
 })
