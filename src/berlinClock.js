6984992edaa7ec2cb9084137fed0c3a59fa11dcb
aurelien-fontana-vinci <aurelien.fontana@student.vinci.be>
2024-11-11 14:08:25 +0100

    add: base project

---
+++ src/berlinClock.js
@@ -0,0 +1,14 @@
+export class Main{
+
+    berlinClock(date){
+        var l1_1 = 0;
+
+        if(date === 1){
+            console.log(l1_1);
+            l1_1 = 1;
+            console.log(l1_1);
+            return l1_1;
+        } 
+
+    }
+}
